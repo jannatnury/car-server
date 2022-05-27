799a760efdde0932ece604c0234b30b1a9baefb4
jannatnury <jannatnury253@gmail.com>
2022-05-28 01:15:54 +0600

    database added

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const express =required('express');
-const cors =required('cors');
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const port =process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000;
 
 // midleware
 app.use(cors());
 app.use(express.json());
 
 
-// root
-app.get('/',(req,res)=>{
+/**
+ * --------------------------------------------------
+ * Config database
+ * --------------------------------------------------
+ * Mongo DB
+ * --------------------------------------------------
+ */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6t6ti.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        /**
+         * --------------------------------------------------
+         * Connect to MongoDB
+         * --------------------------------------------------
+         */
+        await client.connect();
+        const productCollection = client.db('car-doctor').collection('products');
+
+    }
+    finally {
+        /**
+         * --------------------------------------------------
+         * disconnect from MongoDB
+         * --------------------------------------------------
+         */
+        // client.close();
+    }
+};
+
+run().catch(console.dir);
+
+/**
+* --------------------------------------------------
+* root
+* --------------------------------------------------
+*/
+
+app.get('/', (req, res) => {
     res.send("manufacturer server is running..");
 });
 
-app.listen(port,()=>{
-    console.log("Server is running on port",port);
+app.listen(port, () => {
+    console.log(`Server started on port... ${port}`);
 });
