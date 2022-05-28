24f8c60c46fdb31d6c6e09cc5a080f6b978dd47a
jannatnury <jannatnury253@gmail.com>
2022-05-28 12:01:46 +0600

    heroku added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6t6ti.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
         /**
