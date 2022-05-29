7ac9a17e502e17c398c703e9eeb92ed93e5e5d48
jannatnury <jannatnury253@gmail.com>
2022-05-29 17:08:54 +0600

    jwt added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // midleware
@@ -32,6 +33,30 @@ async function run() {
          */
         await client.connect();
         const productCollection = client.db('car-doctor').collection('products');
+        const orders = client.db('car-doctor').collection('orders');
+        const reviewCollection = client.db('car-doctor').collection('reviews');
+        const users = client.db('car-doctor').collection('users');
+        const admins = client.db('car-doctor').collection('admins');
+        const profile = client.db('car-doctor').collection('profile');
+        const projects = client.db('car-doctor').collection('projects');
+
+
+        app.put('/api/login', async (req, res) => {
+
+            const email = req.body.email;
+            console.log(email);
+            const filter = { email: email };
+            const user = { email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await users.updateOne(filter, updateDoc, options);
+            const accessToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '3d'
+            });
+            res.send({ result, accessToken });
+        })
 
         /**
          * --------------------------------------------------
@@ -45,7 +70,47 @@ async function run() {
             const products = await productCollection.find(cursor).toArray();
             res.send(products);
         });
-    
+
+        app.get('/api/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const product = await productCollection.findOne({ _id: ObjectId(id) });
+            res.send(product);
+        })
+        // order
+        app.post('/api/order', async (req, res) => {
+            const order = req.body;
+            console.log(order);
+            const product = await productCollection.findOne({ _id: ObjectId(order?.product) })
+            if (product) {
+
+                let qtn = parseInt(product?.quantity) - parseInt(order?.quantity);
+                const pay = parseFloat(product?.price) * parseInt(order?.quantity);
+                console.log(parseInt(order?.quantity))
+                console.log(parseInt(product?.price));
+                console.log(parseInt(order?.quantity));
+                console.log(parseInt(product?.quantity));
+                console.log(pay);
+                if (pay < 999999) {
+                    order.pay = pay;
+                    await productCollection.updateOne({ _id: ObjectId(order?.product) }, { $set: { quantity: qtn } });
+
+                    const result = await orders.insertOne(order);
+                    return res.send(result);
+                }
+                else {
+                    return res.send({ message: `you can't order more than $999,999.99` });
+                }
+
+            }
+            res.send({ message: 'Product not found' });
+
+        })
+        app.get('/api/orders', async (req, res) => {
+            const email = req.query.email;
+            const result = await orders.find({ user: email }).toArray();
+            res.send(result);
+        })
+
     }
     finally {
         /**
